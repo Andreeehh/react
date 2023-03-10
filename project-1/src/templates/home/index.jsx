897facade2463829ed467203fc66cc11f8fb9d31
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-10 15:35:51 -0300

    separação dos estilos e adicionar botão para manipular dados na tela

--- project-1/src/templates/home/index.jsx
+++ project-1/src/templates/home/index.jsx
@@ -4,10 +4,14 @@ import "./styles.css";
 
 import { loadPosts } from "../../utils/load-post";
 import { Posts } from "../../components/Posts";
+import { Button } from "../../components/Button";
 
 export class Home extends Component {
   state = {
     posts: [],
+    allPosts: [],
+    page: 0,
+    postPerPage: 2,
   };
 
   async componentDidMount() {
@@ -15,15 +19,35 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postPerPage } = this.state;
     const postsAndPhotos = await loadPosts();
-    this.setState({ posts: postsAndPhotos });
+    this.setState({
+      posts: postsAndPhotos.slice(page, postPerPage),
+      allPosts: postsAndPhotos,
+    });
+  };
+
+  loadMorePosts = () => {
+    const { page, postPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage);
+    posts.push(...nextPosts);
+    this.setState({ posts, page: nextPage });
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, page, allPosts } = this.state;
+    const noMorePosts = page >= allPosts.length;
     return (
       <section className="container">
         <Posts posts={posts} />
+        <div class="button-container">
+          <Button
+            text="Load more posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </div>
       </section>
     );
   }
