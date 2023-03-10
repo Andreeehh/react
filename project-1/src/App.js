a0070376c7a14211df4f1b2657300af59b62e8a8
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-10 10:20:04 -0300

    criando classes component e lidando com eventos de click para mudar state

---
+++ project-1/src/App.js
@@ -0,0 +1,67 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    name: "André Carvalho",
+    counter: 0,
+  };
+
+  handlePClick = () => {
+    this.setState({ name: "Júnior" }); //sempre q o state mudar o rende será chamado novamente
+  };
+
+  handleAClick = (event) => {
+    //por ser arrowFunction o bind não é necessário, pois arrow function n tem this, e pega do this acima
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
+  render() {
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Link a
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
