047a7200e85f34259d4d3b41d2f6842a51c9f3d0
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-10 16:13:58 -0300

    TextInput

--- project-1/src/templates/home/index.jsx
+++ project-1/src/templates/home/index.jsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import { loadPosts } from "../../utils/load-post";
 import { Posts } from "../../components/Posts";
 import { Button } from "../../components/Button";
+import { TextInput } from "../../components/TextInput";
 
 export class Home extends Component {
   state = {
@@ -12,6 +13,7 @@ export class Home extends Component {
     allPosts: [],
     page: 0,
     postPerPage: 2,
+    searchValue: "",
   };
 
   async componentDidMount() {
@@ -35,18 +37,45 @@ export class Home extends Component {
     this.setState({ posts, page: nextPage });
   };
 
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
+  };
+
   render() {
-    const { posts, page, allPosts } = this.state;
-    const noMorePosts = page >= allPosts.length;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue
+      ? allPosts.filter((post) => {
+          return post.title.toLowerCase().includes(searchValue.toLowerCase());
+        })
+      : posts;
+
     return (
       <section className="container">
-        <Posts posts={posts} />
-        <div className="button-container">
-          <Button
-            text="Load more posts"
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
+        <div className="search-container">
+          {!!searchValue && ( //!! transforma searchValue em um booleano, se tiver busca, faça o que estiver dps do &&
+            <>
+              <h1>Seach value: {searchValue}</h1>
+            </>
+          )}
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
           />
+        </div>
+        {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+        {filteredPosts.length === 0 && <p>Não existem posts</p>}
+
+        <div className="button-container">
+          {!searchValue && ( //se nao tiver busca, exibe o botão
+            <Button
+              text="Load more posts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
